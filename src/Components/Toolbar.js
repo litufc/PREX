4944c5065a89b5a962afedd1eb7f4c36f44a1ca0
Mateus Santos <mateus.pereira99@hotmail.com>
2019-07-08 13:00:45 -0300

    Toolbar com icone de voltar

--- src/Components/Toolbar.js
+++ src/Components/Toolbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Header, Body, Title, Left, Right, Container } from 'native-base';
+import { Header, Body, Title, Left, Right, Container, Icon } from 'native-base';
 
-const Toolbar = ( { title, textColor, background } ) => {
+const Toolbar = ( { title, textColor, background, iconColor } ) => {
     const styles = StyleSheet.create({
         titleHeader: {
             fontFamily: 'Roboto_medium',
@@ -15,12 +15,26 @@ const Toolbar = ( { title, textColor, background } ) => {
             height: 80
         },
         containerTitle: {
-            alignItems: "center"
+            alignItems: "center",
+            marginLeft: -20
+        },
+        leftContainer: {
+            flex: 0,
+            paddingLeft: 10,
+            width: 36
+        },
+        iconBack: {
+            color: iconColor
         }
     });
     return(
         <Container>
         <Header style={styles.header}>
+            <Left style={styles.leftContainer}>
+                {iconColor &&
+                    <Icon type="FontAwesome" name="arrow-left" size={23} style={styles.iconBack}/>
+                }
+            </Left>
             <Body style={styles.containerTitle}>
                 <Title style={styles.titleHeader}>{title}</Title>
             </Body>
--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Container } from 'native-base';
 
+import AppStyles from '../global';
 import Toolbar from '../Components/Toolbar';
 
 export default class Login extends Component {
     render(){
         return(
             <View>
-                <Toolbar title="Menu" textColor="#FFFFFF" background="#006CB4"/>
+                <Toolbar 
+                    title="Menu" 
+                    textColor="#FFFFFF" 
+                    background={AppStyles.colour.primaryColor}
+                />
                 <View style={styles.container}>
                 </View>
             </View>
@@ -24,6 +29,6 @@ const styles = StyleSheet.create({
         height: '100%',
         width: '100%',
         marginTop: 80,
-        backgroundColor: '#006CB4'
+        backgroundColor: AppStyles.colour.primaryColor
     }
 })
