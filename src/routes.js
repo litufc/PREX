24058bd4a4021ec9517e7c71c6cb5b008846ea17
Mateus Santos <mateus.pereira99@hotmail.com>
2019-07-19 11:01:39 -0300

    Página de Status de Agendamento

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import Menu from './Pages/Menu';
 import Schedules from './Pages/Schedules';
 import Options from './Pages/Options';
+import SchedulesStatus from './Pages/ScheduleStatus';
 
 const Routes = createAppContainer(
     createSwitchNavigator({
         Menu,
         Schedules,
         Options,
+        SchedulesStatus,
     })
 );
 
